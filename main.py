e6524ada2ae33c59127f5925738dfd8877815b49
Pranav S <pranav.sampathkumar@gmail.com>
2024-10-15 23:44:07 +0200

    bad commit messages

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+data = np.loadtxt("data.csv")
+plt.plot(np.arange(20), np.poly1d(np.polyfit(data[:,0], data[:,1], 1))(np.arange(20)), color='r')
+plt.scatter(data[:,0], data[:,1])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.savefig("plot.png")
