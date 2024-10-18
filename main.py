50f6d1d10d8f602039193ea590e84a3e6e20722c
sprucemaster <68788737+sprucemaster@users.noreply.github.com>
2024-10-18 16:33:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 data = np.loadtxt("data.csv")
-plt.plot(np.arange(20), np.poly1d(np.polyfit(data[:,0], data[:,1], 1))(np.arange(20)), color='r')
-plt.scatter(data[:,0], data[:,1])
-plt.xlabel('xa')
-plt.ylabel('y')
+plt.plot(
+    np.arange(20),
+    np.poly1d(np.polyfit(data[:, 0], data[:, 1], 1))(np.arange(20)),
+    color="r",
+)
+plt.scatter(data[:, 0], data[:, 1])
+plt.xlabel("xa")
+plt.ylabel("ya")
 plt.savefig("plot.png")
